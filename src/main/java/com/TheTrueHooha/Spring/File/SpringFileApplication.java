1904438ec844389c76f4033c11627aeaf85e7a20
David Ogar <owogogahhero@outlook.com>
2022-03-10 21:14:01 +0100

    moving files to remote

---
+++ src/main/java/com/TheTrueHooha/Spring/File/SpringFileApplication.java
@@ -0,0 +1,13 @@
+package com.TheTrueHooha.Spring.File;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFileApplication.class, args);
+	}
+
+}
